b5bfe7026d6ad4f6480967be42e80551eaf31dac
jbj338033 <jbj338033@gmail.com>
2026-03-31 20:53:47 +0900

    feat: add toast notifications and alert dialog components

--- apps/web/src/app/index.tsx
+++ apps/web/src/app/index.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { AppRouter } from './router'
+import { Toaster } from '@/shared/ui/sonner'
 import './styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AppRouter />
+      <Toaster position="bottom-right" />
     </BrowserRouter>
   </StrictMode>,
 )
